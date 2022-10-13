3d6a4741123f862e63dae3dd4a08327ae7af98b3
Uladzislau_Komar <vladkomar25@gmail.com>
2022-10-13 20:24:40 +0300

    Created UI Launch Screen

---
+++ Coffee Been/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Coffee Been
+//
+//  Created by Uladzislau Komar on 13.10.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
