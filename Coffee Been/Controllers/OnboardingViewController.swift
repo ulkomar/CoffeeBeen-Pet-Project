b80308a69549de1a9052e08f911de907324a4bdb
Uladzislau_Komar <vladkomar25@gmail.com>
2022-11-02 19:13:21 +0300

    Structured files

---
+++ Coffee Been/Controllers/OnboardingViewController.swift
@@ -0,0 +1,17 @@
+//
+//  OnboardingViewController.swift
+//  Coffee Been
+//
+//  Created by Uladzislau Komar on 2.11.22.
+//
+
+import UIKit
+
+class OnboardingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = .red
+    }
+
+}
