c9533eba86b9d52e51abc5d93d7b7b8dfa8d2ea6
Uladzislau_Komar <vladkomar25@gmail.com>
2022-11-03 18:32:04 +0300

    Property refactoring

--- Coffee Been/Controllers/OnboardingViewController.swift
+++ Coffee Been/Controllers/OnboardingViewController.swift
@@ -9,33 +9,42 @@ import UIKit
 
 class OnboardingViewController: UIViewController {
     
+    //MARK: - Properties
+    
     var screenInformation = OnboardingInfo()
-    var scrollView = UIScrollView()
-    var stackView = UIStackView()
+    let pageNumbers = 3
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        scrollView = setupScrollView(mainView: self.view)
-        self.view.addSubview(scrollView)
-        
-        addShadow(toView: view)
-        
-        
-    }
+    //MARK: - UIView setups
     
-    private func setupStackView() {
+    lazy var scrollView: UIScrollView = {
+        let scrollView = UIScrollView()
+        let width = view.bounds.width
+        let height = view.bounds.height
+        let frame = CGRect(x: 0, y: 0, width: width, height: height)
+        
+        scrollView.frame = view.frame
+        scrollView.contentSize = CGSize(width: width * CGFloat(pageNumbers), height: height)
+        scrollView.isPagingEnabled = true
+        scrollView.backgroundColor = .gray
+        
+        return scrollView
+    }()
+    
+    lazy var stackView: UIStackView = {
+        let stackView = UIStackView()
+        
         let label = UILabel()
         label.text = screenInformation.getScreenInformation(index: 0)[1]
         label.font = UIFont.systemFont(ofSize: 20, weight: .medium)
         label.textColor = .white
         label.numberOfLines = 3
-        
+
         let label1 = UILabel()
         label1.text = screenInformation.getScreenInformation(index: 0)[0]
         label1.font = UIFont.systemFont(ofSize: 50, weight: .semibold)
         label1.textColor = .white
         label1.numberOfLines = 3
-        
+
         stackView.translatesAutoresizingMaskIntoConstraints = false
         stackView.center = self.view.center
         stackView.axis = .vertical
@@ -44,79 +53,61 @@ class OnboardingViewController: UIViewController {
         stackView.addArrangedSubview(label1)
         stackView.addArrangedSubview(label)
         
-        
+        return stackView
+    }()
+    
+    //MARK: - Lifecycle
+   
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupSubviews()
+        setupConstraints()
     }
     
-    private func setupScrollView(mainView main: UIView) -> UIScrollView {
-        // settings
-        let view = UIScrollView()
-        let width = main.bounds.width
-        let height = main.bounds.height
-        let frame = CGRect(x: 0, y: 0, width: width, height: height)
-        let pageNumbers = 3
+    //MARK: - Private methods
+    
+    private func setupSubviews() {
+        // set up scrolling pages
+        self.view.addSubview(scrollView)
+        setupPage(toScrollView: scrollView, toPage: 0, imageName: "onboarding1")
+        setupPage(toScrollView: scrollView, toPage: 1, imageName: "onboarding2")
+        setupPage(toScrollView: scrollView, toPage: 2, imageName: "onboarding3")
         
-        view.frame = frame
-        view.contentSize = CGSize(width: width * CGFloat(pageNumbers), height: height)
-        view.isPagingEnabled = true
-        view.backgroundColor = .gray
-        
-        self.setupFirstPage(scrollView: view)
-        self.setupSecondPage(scrollView: view)
-        self.setupThirdPage(scrollView: view)
-        return view
+        self.view.addSubview(stackView)
     }
     
-    private func setupFirstPage(scrollView: UIScrollView) {
-        let frame = CGRect(x: 0, y: 0, width: scrollView.bounds.width, height: scrollView.bounds.height)
+    private func setupPage(toScrollView scView: UIScrollView, toPage pageNum: Int, imageName image: String) {
+        let frame = CGRect(x: scView.frame.width * CGFloat(pageNum), y: 0, width: scView.frame.width, height: scView.frame.height)
         let page = UIImageView(frame: frame)
-        page.image = UIImage(named: "onboarding4")
+        page.image = UIImage(named: image)
         page.contentMode = .scaleAspectFill
         page.clipsToBounds = true
-        scrollView.addSubview(page)
-        
-        
-        page.addSubview(stackView)
-        setupStackView()
+        scView.addSubview(page)
+    }
+    
+    private func setupConstraints() {
         NSLayoutConstraint.activate([
-            stackView.bottomAnchor.constraint(equalTo: page.safeAreaLayoutGuide.bottomAnchor, constant: 0),
-            stackView.leadingAnchor.constraint(equalTo: page.leadingAnchor, constant: 20),
-            stackView.trailingAnchor.constraint(equalTo: page.trailingAnchor, constant: 20)
+            stackView.bottomAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.bottomAnchor),
+            stackView.leadingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.leadingAnchor, constant: 20),
+            stackView.trailingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.trailingAnchor, constant: -20)
         ])
     }
     
-    private func setupSecondPage(scrollView: UIScrollView) {
-        let frame = CGRect(x: scrollView.bounds.width, y: 0, width: scrollView.bounds.width, height: scrollView.bounds.height)
-        let page = UIImageView(frame: frame)
-        page.image = UIImage(named: "onboarding2")
-        page.contentMode = .scaleAspectFill
-        page.clipsToBounds = true
-        scrollView.addSubview(page)
-    }
-    
-    private func setupThirdPage(scrollView: UIScrollView) {
-        let frame = CGRect(x: scrollView.bounds.width * 2, y: 0, width: scrollView.bounds.width, height: scrollView.bounds.height)
-        let page = UIImageView(frame: frame)
-        page.image = UIImage(named: "onboarding3")
-        page.contentMode = .scaleAspectFill
-        page.clipsToBounds = true
-        scrollView.addSubview(page)
-    }
-    
-    private func addShadow(toView: UIView) {
-//        let layer = CALayer()
-//        layer.backgroundColor = UIColor.black.cgColor
+//    private func addShadow(toView: UIView) {
+////        let layer = CALayer()
+////        layer.backgroundColor = UIColor.black.cgColor
+////        layer.frame = toView.frame
+////        layer.opacity = 0.3
+//        let topColor = UIColor.clear.withAlphaComponent(0.1).cgColor
+//        let bottomColor = UIColor.black.withAlphaComponent(0.7).cgColor
+//        let layer = CAGradientLayer()
+//        layer.startPoint = CGPoint(x: 0.5, y: 0.0)
+//        layer.endPoint = CGPoint(x: 0.5, y: 0.9)
 //        layer.frame = toView.frame
-//        layer.opacity = 0.3
-        let topColor = UIColor.clear.withAlphaComponent(0.1).cgColor
-        let bottomColor = UIColor.black.withAlphaComponent(0.7).cgColor
-        let layer = CAGradientLayer()
-        layer.startPoint = CGPoint(x: 0.5, y: 0.0)
-        layer.endPoint = CGPoint(x: 0.5, y: 0.9)
-        layer.frame = toView.frame
-        layer.colors = [topColor, bottomColor]
-        layer.transform = CATransform3DMakeRotation(0, 0, 0, 0)
-        toView.layer.addSublayer(layer)
-    }
+//        layer.colors = [topColor, bottomColor]
+//        layer.transform = CATransform3DMakeRotation(0, 0, 0, 0)
+//        toView.layer.addSublayer(layer)
+//    }
     
 
 }
